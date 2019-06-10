7b1ed18646df6660c6591c93910185f3eee00f15
AKhegay <akhegay@beeline.kg>
2019-06-10 16:36:34 +0600

    - build & increment version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,26 @@
 export declare type ActionType = string;
+/**
+ * Redux Action
+ */
 export interface Action {
     type: ActionType;
     [key: string]: any;
 }
-export declare type Reducer<State> = (state: State, action?: Action) => State;
+/**
+ * Reducer type
+ */
+export declare type Reducer<State> = (state: State, action?: Action) => Partial<State>;
+/**
+ * Function filtering actions
+ */
 export declare type ReducerFilter<State> = {
     filter?: (type: ActionType, action: Action) => boolean;
     type?: ActionType[] | ActionType;
     reduce: Reducer<State>;
 };
+/**
+ *
+ * @param reducerFilters Array of Reducer
+ * @param initialState Initial Redux state
+ */
 export declare const makeReducerHandler: <State>(reducerFilters: ReducerFilter<State>[], initialState: State) => (state: State | undefined, action: Action) => State;
